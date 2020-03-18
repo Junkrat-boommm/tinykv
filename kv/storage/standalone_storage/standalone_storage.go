1f3d1c3687b84e13b910b8cd31bf96f2f7b6e3df
junkman <32281904+Junkrat-boommm@users.noreply.github.com>
2020-03-18 17:22:54 +0800

    简单的测试使用github

--- kv/storage/standalone_storage/standalone_storage.go
+++ kv/storage/standalone_storage/standalone_storage.go
@@ -4,21 +4,27 @@ import (
 	"github.com/pingcap-incubator/tinykv/kv/config"
 	"github.com/pingcap-incubator/tinykv/kv/storage"
 	"github.com/pingcap-incubator/tinykv/proto/pkg/kvrpcpb"
+	"github.com/Connor1996/badger"
+	"github.com/pingcap-incubator/tinykv/util/engine_util"
 )
 
 // StandAloneStorage is an implementation of `Storage` for a single-node TinyKV instance. It does not
 // communicate with other nodes and all data is stored locally.
 type StandAloneStorage struct {
 	// Your Data Here (1).
+	db *badger.DB
 }
 
 func NewStandAloneStorage(conf *config.Config) *StandAloneStorage {
 	// Your Code Here (1).
-	return nil
+	// interact with badger through `engine_util` API
+	db := engine_util.CreateDB("sa_storage", Config)
+	return StandAloneStorage{db: db}
 }
 
 func (s *StandAloneStorage) Start() error {
 	// Your Code Here (1).
+	s.db.st
 	return nil
 }
 
